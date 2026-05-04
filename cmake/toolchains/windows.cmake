c90fd578b75577f71677e1dfe68be1dbf69567e7
ryry2nd <ryry2nd@gmail.com>
2026-05-04 17:35:06 -0400

    fixing windows

--- cmake/toolchains/windows.cmake
+++ cmake/toolchains/windows.cmake
@@ -1,7 +1,6 @@
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+set(CMAKE_C_COMPILER ccache clang)
+set(CMAKE_CXX_COMPILER ccache clang++)
 
 # if your a crackhead and want to do something stupid like change the supported api's then go ahead, but remember it may or may not work
 # keep in mind if you change these after you already build it, you gotta delete your build folder and spend 30 minutes compiling again. btw good luck
