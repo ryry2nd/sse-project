51a07db335a0e9857691aa746fd4647d256e1190
ryry2nd <ryry2nd@gmail.com>
2026-05-27 14:24:11 -0400

    windows compatibility

--- cmake/toolchains/windows.cmake
+++ cmake/toolchains/windows.cmake
@@ -1,10 +1,11 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_C_COMPILER clang-22)
+# make sure it is clang 22
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET x86_64-w64-windows-gnu)
 set(CMAKE_C_COMPILER_LAUNCHER ccache)
-set(CMAKE_CXX_COMPILER clang++-22)
+set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET x86_64-w64-windows-gnu)
 set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
 
