f5c7b4dd83a70f26499a412960bb43a9f280f948
ryry2nd <ryry2nd@gmail.com>
2026-05-27 17:28:17 -0400

    thing

--- setup.sh
+++ setup.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-sudo apt update
+OS="linux"
 
-sudo apt install -y cmake \
-    ninja-build \
-    python3 \
-    python3-venv \
-    clang-22 \
-    llvm-22 \
-    ccache \
-    xorg-dev \
-    build-essential \
-    libzstd-dev \
-	nasm
+while [[ $# -gt 0 ]]; do
+  case $1 in
+	--os)
+	  OS="$2"
+	  shift 2
+	  ;;
+	*)
+	  echo "Unknown option $1"
+	  exit 1
+	  ;;
+  esac
+done
 
-# pacman -S cmake clang llvm ccache ninja nasm python3
+if [ "$OS" = "linux" ]; then
+	sudo apt update
+
+	sudo apt install -y cmake \
+		ninja-build \
+		python3 \
+		python3-venv \
+		clang-22 \
+		llvm-22 \
+		ccache \
+		xorg-dev \
+		build-essential \
+		libzstd-dev \
+		nasm
+elif [ "$OS" = "windows" ]; then
+	# this doesn't even work yet. I hate windows
+	pacman -S --noconfirm cmake clang llvm ccache ninja nasm python3 zlib
+fi
