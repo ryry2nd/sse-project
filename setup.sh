ea0c90228e1daa0ff4f84c33b83bd4cb4d60c4f6
ryry2nd <ryry2nd@gmail.com>
2026-05-26 21:22:13 -0400

    fixes

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 sudo apt update
 
-sudo apt install cmake \
+sudo apt install -y cmake \
     ninja-build \
     python3 \
     python3-venv \
     clang-20 \
-    llvm-20
+    llvm-20 \
     ccache \
     xorg-dev \
     build-essential \
