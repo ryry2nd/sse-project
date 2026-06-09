a000881fac4344089f696318a76d9196ea691a9c
ryry2nd <ryry2nd@gmail.com>
2026-06-08 23:12:03 -0400

    fixed genmesh

--- src/modules/example1/src/GenMesh.hpp
+++ src/modules/example1/src/GenMesh.hpp
@@ -4,6 +4,22 @@
 
 using namespace Engine::Rendering;
 
+#define isSolid(x, y, z) (x < 0 || y < 0 || z < 0 || x >= size.x || y >= size.y || z >= size.z)
+
+#define face(a, b, c, d, n) \
+{ \
+	Ind start = (Ind)vertices.size(); \
+	vertices.push_back({a, {0,0}, n}); \
+	vertices.push_back({b, {1,0}, n}); \
+	vertices.push_back({c, {1,1}, n}); \
+	vertices.push_back({d, {0,1}, n}); \
+	indices.push_back(start+0); \
+	indices.push_back(start+1); \
+	indices.push_back(start+2); \
+	indices.push_back(start+2); \
+	indices.push_back(start+3); \
+	indices.push_back(start+0); \
+}
 
 void buildVoxelGrid(std::vector<Vert>& vertices,
                     std::vector<Ind>& indices,
@@ -19,29 +35,6 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 	vertices.reserve(surfaceApprox * 4);
 	indices.reserve(surfaceApprox * 6);
 
-    auto isSolid = [&](int x, int y, int z) -> bool
-    {
-        if (x < 0 || y < 0 || z < 0 ||
-            x >= size.x || y >= size.y || z >= size.z)
-            return false;
-        return true; // replace later with real voxel data
-    };
-
-    auto face = [&](glm::vec3 a, glm::vec3 b, glm::vec3 c, glm::vec3 d, glm::vec3 n)
-    {
-        Ind start = (Ind)vertices.size();
-
-        vertices.push_back({a, {0,0}, n});
-        vertices.push_back({b, {1,0}, n});
-        vertices.push_back({c, {1,1}, n});
-        vertices.push_back({d, {0,1}, n});
-
-        indices.insert(indices.end(), {
-            start+0, start+1, start+2,
-            start+2, start+3, start+0
-        });
-    };
-
     for (int x = 0; x < size.x; x++)
     for (int y = 0; y < size.y; y++)
     for (int z = 0; z < size.z; z++)
@@ -56,7 +49,7 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 			p+glm::vec3(1,1,0),
 			p+glm::vec3(1,1,1),
 			p+glm::vec3(1,0,1),
-			{1,0,0}
+			glm::vec3(1,0,0)
 		);
 
 		if (!isSolid(x-1,y,z))
@@ -65,7 +58,7 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 			p+glm::vec3(0,0,1),
 			p+glm::vec3(0,1,1),
 			p+glm::vec3(0,1,0),
-			{-1,0,0}
+			glm::vec3(-1,0,0)
 		);
 
 		if (!isSolid(x,y+1,z))
@@ -74,7 +67,7 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 			p+glm::vec3(0,1,1),
 			p+glm::vec3(1,1,1),
 			p+glm::vec3(1,1,0),
-			{0,1,0}
+			glm::vec3(0,1,0)
 		);
 
 		if (!isSolid(x,y-1,z))
@@ -83,7 +76,7 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 			p+glm::vec3(1,0,0),
 			p+glm::vec3(1,0,1),
 			p+glm::vec3(0,0,1),
-			{0,-1,0}
+			glm::vec3(0,-1,0)
 		);
 
 		if (!isSolid(x,y,z+1))
@@ -92,7 +85,7 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 			p+glm::vec3(1,0,1),
 			p+glm::vec3(1,1,1),
 			p+glm::vec3(0,1,1),
-			{0,0,1}
+			glm::vec3(0,0,1)
 		);
 
 		if (!isSolid(x,y,z-1))
@@ -101,7 +94,7 @@ void buildVoxelGrid(std::vector<Vert>& vertices,
 			p+glm::vec3(0,1,0),
 			p+glm::vec3(1,1,0),
 			p+glm::vec3(1,0,0),
-			{0,0,-1}
+			glm::vec3(0,0,-1)
 		);
     }
 }
